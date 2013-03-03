bd38bfa4e6e0c3ae1a271fc9ac85d89e7e7d8340
niaeashes <nia.eashes@gmail.com>
2013-03-04 06:27:24 +0900

    Create fuelbook::comments view.

---
+++ views/comments.php
@@ -0,0 +1,2 @@
+<?php echo \View::forge('fuelbook::root'); ?>
+<div class="fb-comments" data-href="<?php echo $url ?>" data-width="<?php echo $width ?>" data-num-posts="<?php echo $num_post ?>"></div>
