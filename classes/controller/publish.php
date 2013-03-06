70fc306c66fe5d764d36bb5a77c3beeaba1d9809
niaeashes <nia.eashes@gmail.com>
2013-03-06 16:36:22 +0900

    Add publish controller

---
+++ classes/controller/publish.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Fuelbook;
+
+class Controller_Publish extends Controller {
+
+	public function action_status($message)
+	{
+		if ( ! \Request::is_hmvc() and \Config::get('fuelbook.only_hmvc'))
+		{
+			throw new HttpNotFoundException();
+		}
+
+		try
+		{
+			Facebook::api('/me/feed', 'POST', array('message' => $message));
+		}
+		catch (\FacebookApiException $e)
+		{
+			\Log::error($e->getMessage());
+		}
+	}
+}
